c20a450b90ddaf27293577da8bdb4c5f8150826b
kda <kuzin_dmitriy@bk.ru>
2019-01-14 20:27:07 +0300

    Breadcrumb

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <nav class="breadcrumb" aria-label="breadcrumbs">
-            <ul>
-                <li><a href="#">Bulma</a></li>
-                <li><a href="#">Documentation</a></li>
-                <li><a href="#">Components</a></li>
-                <li class="is-active"><a href="#" aria-current="page">Breadcrumb</a></li>
-            </ul>
-        </nav>
-    </div>
+
+    @include('includes.breadcrumb')
+
     <div class="container container_auth">
         <div class="columns">
             <div class="column">
---
+++ resources/views/includes/breadcrumb.blade.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <nav class="breadcrumb" aria-label="breadcrumbs">
+        <ul>
+            <li><a href="#"><span>Bulma</span></a></li>
+            <li><a href="#"><span>Documentation</span></a></li>
+            <li><a href="#"><span>Components</span></a></li>
+            <li class="is-active"><a href="#" aria-current="page"><span>Breadcrumb</span></a></li>
+        </ul>
+    </nav>
+</div>
