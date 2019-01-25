ec483376db466c735aca4828aea48c65ee17443c
Kuzin Dmitry <kuzin_dmitriy@bk.ru>
2019-01-25 10:27:45 +0300

    Static card service on page /spa/services and /business/single

--- resources/views/spa/services.blade.php
+++ resources/views/spa/services.blade.php
@@ -35,22 +35,142 @@
                     </div>
                     <div class="columns is-multiline">
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                         <div class="column is-6">
-                            @include('parts.card-service')
+                            <!-- Example Layout card service -->
+                            <div class="card card-service">
+                                <div class="card-service__body">
+                                    <div class="is-flex">
+                                        <figure class="card-service__icon">
+                                            <img src="{{ asset('/svg/icons/services/ic_translate.svg') }}" alt="">
+                                        </figure>
+                                        <h4 class="card-service__title">Перевод документов</h4>
+                                    </div>
+                                    <div class="card-service__info">
+                                        <div class="card-service__excerpt is-size-875">
+                                            Наши специалисты переведут документы на английский и русский.
+                                        </div>
+                                        <div class="card-service__price">€15 за один перевод</div>
+                                    </div>
+                                </div>
+                                <div class="card-service__footer">
+                                    <a class="button is-warning is-outlined is-size-875 h-3">Заказать</a>
+                                    <a class="button is-clear is-clear_close is-size-875 h-3"><span class="has-text-decoration-underline">Подробнее</span></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
