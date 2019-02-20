03fbbfcfcfd8a382203afa0175096e6b70b5cf41
Nick Kuznetsov <pixelfixer74@gmail.com>
2019-02-20 13:59:31 +0500

    fixes

--- resources/js/components/profile/views/purchased-service.vue
+++ resources/js/components/profile/views/purchased-service.vue
@@ -1,20 +1,31 @@
 <template>
-    <section class="section pt-1 px-0">
+    <section class="section pt-1 px-0" v-if="service">
         <h1 class="section-title mb-1-75">
-            <img src="/svg/icons/services/ic_spy.svg" alt="">
-            <span>{{service.name}}</span>
+            <img :src="service.service.icon" alt="">
+            <span>{{ $t(service.service.name) }}</span>
         </h1>
+        <p>Процесс заказанной услуги</p>
     </section>
 </template>
 
 <script>
 export default {
-    props: ['service'],
+    // props: ['service'],
+    data:()=> ({
+        service: null
+    }),
     mounted(){
-        console.log(this.$route)
+        this.getService();
     },
-    getService(){
-        ax
+    methods:{
+        getService(){
+            axios.get('/profile/api/purchased_service/' + this.$route.params.id)
+            .then( res => {
+                this.service = res.data;
+            }).catch( e => {
+                this.$swal({ type: 'error', title: e.response.status, text: e.response.data.message });
+            })
+        }
     }
 }
 </script>
