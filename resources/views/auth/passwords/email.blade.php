01086d9843b7e7ad45f7265962273f07c0244207
kda <kuzin_dmitriy@bk.ru>
2019-03-07 21:43:20 +0300

    Responsive auth

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -2,71 +2,74 @@
 
 @section('content')
 
-
-    <div class="container container_auth">
-        <div class="columns">
-            <div class="column">
-                <div class="card-login card-auth box">
-                    <div class="card-auth__header">
-                        <div class="card-auth__header__title has-text-centered">
-                            <h1>{{ __('Reset Password') }}</h1>
+    <section class="section py-0 is-flex is-flex-1">
+        <div class="container container_auth">
+            <div class="columns">
+                <div class="column">
+                    <div class="card-login card-auth box">
+                        <div class="card-auth__header">
+                            <div class="card-auth__header__title has-text-centered">
+                                <h1>{{ __('Reset Password') }}</h1>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body card-auth__body">
-                        @if (session('status'))
-                            <div class="notification-list">
-                                <div class="notification is-success">
-                                    <div class="notification__icon">
-                                        <img src="{{ asset('/svg/icons/notification/ic_thankyou.svg') }}" class="svg"
-                                             alt="">
-                                    </div>
-                                    <div class="notification__content-wrap">
-                                        <h5 class="notification__title has-text-weight-bold">
-                                            {{ session('status') }}
-                                        </h5>
+                        <div class="card-body card-auth__body">
+                            @if (session('status'))
+                                <div class="notification-list">
+                                    <div class="notification is-success">
+                                        <div class="notification__icon">
+                                            <img src="{{ asset('/svg/icons/notification/ic_thankyou.svg') }}"
+                                                 class="svg"
+                                                 alt="">
+                                        </div>
+                                        <div class="notification__content-wrap">
+                                            <h5 class="notification__title has-text-weight-bold">
+                                                {{ session('status') }}
+                                            </h5>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endif
+                            @endif
 
 
-                        <form method="POST" action="{{ route('password.email') }}">
-                            @csrf
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
 
-                            <div class="field">
-                                <label for="email" class="label">{{ __('E-Mail Address') }}</label>
-                                <div class="control">
-                                    <input id="email" type="email"
-                                           class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email"
-                                           value="{{ old('email') }}" required autofocus>
+                                <div class="field">
+                                    <label for="email" class="label">{{ __('E-Mail Address') }}</label>
+                                    <div class="control">
+                                        <input id="email" type="email"
+                                               class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email"
+                                               value="{{ old('email') }}" required autofocus>
 
-                                    @if ($errors->has('email'))
-                                        <span class="help is-danger" role="alert">
+                                        @if ($errors->has('email'))
+                                            <span class="help is-danger" role="alert">
                                         {{ $errors->first('email') }}
                                     </span>
-                                    @endif
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
 
 
-                            <div class="field">
-                                <div class="control">
-                                    <button type="submit" class="button is-info is-fullwidth has-text-weight-bold">
-                                        {{ __('Send Password Reset Link') }}
-                                    </button>
+                                <div class="field">
+                                    <div class="control">
+                                        <button type="submit"
+                                                class="button is-info is-fullwidth has-text-weight-bold h-3">
+                                            {{ __('Send Password Reset Link') }}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="has-text-centered">
-                                <a class="has-text-info is-link" href="{{ route('login') }}">
-                                    Вспомнили пароль?
-                                </a>
-                            </div>
+                                <div class="has-text-centered">
+                                    <a class="has-text-info is-link" href="{{ route('login') }}">
+                                        Вспомнили пароль?
+                                    </a>
+                                </div>
 
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -2,72 +2,75 @@
 
 @section('content')
 
-    <div class="container container_auth">
-        <div class="columns">
-            <div class="column">
-                <div class="card-login card-auth box">
-                    <div class="card-auth__header">
-                        <div class="card-auth__header__title has-text-centered">
-                            <h1>{{ __('Reset Password') }}</h1>
+    <section class="section py-0 is-flex is-flex-1">
+        <div class="container container_auth">
+            <div class="columns">
+                <div class="column">
+                    <div class="card-login card-auth box">
+                        <div class="card-auth__header">
+                            <div class="card-auth__header__title has-text-centered">
+                                <h1>{{ __('Reset Password') }}</h1>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body card-auth__body">
-                        <form method="POST" action="{{ route('password.update') }}">
-                            @csrf
+                        <div class="card-body card-auth__body">
+                            <form method="POST" action="{{ route('password.update') }}">
+                                @csrf
 
-                            <input type="hidden" name="token" value="{{ $token }}">
+                                <input type="hidden" name="token" value="{{ $token }}">
 
-                            <div class="field">
-                                <label for="email" class="label">{{ __('E-Mail Address') }}</label>
-                                <div class="control">
-                                    <input id="email" type="email"
-                                           class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email"
-                                           value="{{ $email ?? old('email') }}" required autofocus>
+                                <div class="field">
+                                    <label for="email" class="label">{{ __('E-Mail Address') }}</label>
+                                    <div class="control">
+                                        <input id="email" type="email"
+                                               class="input{{ $errors->has('email') ? ' is-danger' : '' }}" name="email"
+                                               value="{{ $email ?? old('email') }}" required autofocus>
 
-                                    @if ($errors->has('email'))
-                                        <span class="help is-danger" role="alert">
+                                        @if ($errors->has('email'))
+                                            <span class="help is-danger" role="alert">
                                         {{ $errors->first('email') }}
                                     </span>
-                                    @endif
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="field">
-                                <label for="password" class="label">{{ __('Password') }}</label>
-                                <div class="control">
-                                    <input id="password" type="password"
-                                           class="input{{ $errors->has('password') ? ' is-danger' : '' }}"
-                                           name="password" required>
+                                <div class="field">
+                                    <label for="password" class="label">{{ __('Password') }}</label>
+                                    <div class="control">
+                                        <input id="password" type="password"
+                                               class="input{{ $errors->has('password') ? ' is-danger' : '' }}"
+                                               name="password" required>
 
-                                    @if ($errors->has('password'))
-                                        <span class="help is-danger" role="alert">
+                                        @if ($errors->has('password'))
+                                            <span class="help is-danger" role="alert">
                                         {{ $errors->first('password') }}
                                     </span>
-                                    @endif
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="field">
-                                <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
-                                <div class="control">
-                                    <input id="password-confirm" type="password"
-                                           class="input{{ $errors->has('password') ? ' is-danger' : '' }}"
-                                           name="password_confirmation" required>
+                                <div class="field">
+                                    <label for="password-confirm" class="label">{{ __('Confirm Password') }}</label>
+                                    <div class="control">
+                                        <input id="password-confirm" type="password"
+                                               class="input{{ $errors->has('password') ? ' is-danger' : '' }}"
+                                               name="password_confirmation" required>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div class="field">
-                                <div class="control">
-                                    <button type="submit" class="button is-info is-fullwidth has-text-weight-bold">
-                                        {{ __('Reset Password') }}
-                                    </button>
+                                <div class="field">
+                                    <div class="control">
+                                        <button type="submit"
+                                                class="button is-info is-fullwidth has-text-weight-bold h-3">
+                                            {{ __('Reset Password') }}
+                                        </button>
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
 @endsection
